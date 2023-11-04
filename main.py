634ca62967de19780af71c3ded53d0df519fb801
JhonDX <149755482+JhonDX@users.noreply.github.com>
2023-11-04 17:44:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from tkinter import *
 
-
+import connect
+from connect import *
 
 class app():
     def __init__(self):
         self.janela = Tk()
+        self.corfundo = '#696969'
+
+        self.janela['bg'] = self.corfundo
 
         #titulo
         self.janela.title('Login')
@@ -16,14 +20,54 @@ class app():
         self.janela.geometry('250x250')
 
         #USAURIO
-        Label(self.janela, text='Usuário :').grid(row=1,column=1)
+        Label(self.janela, text='Usuário :', font='calibri', bg=self.corfundo).place(x=10,y=30)
+        self.user = Entry(self.janela)
+        self.user.place(x=75,y=31)
 
-        Entry(self.janela, show='*').grid(row=1,column=2)
+        #SENHA
+        Label(self.janela, text='  Senha  :', font='calibri', bg=self.corfundo).place(x=10,y=80)
+        self.passw = Entry(self.janela, show='*')
+        self.passw.place(x=75,y=81)
+
+
+        #BUTTON
+
+        Button(self.janela, command=self.login, text='Entrar', font='calibri', bg='green', fg='white').place(x=100,y=120)
+
 
         #BOTÃO SAIR
-        Button(self.janela, text='Sair', command=self.janela.destroy).grid(row=0, column=0)
+        Button(self.janela, text='Sair', command=self.janela.destroy, bg='red', fg='white', font='calibri').place(x=108,y=180)
 
 
         self.janela.mainloop()
+
+    def login(self):
+        self.usuario = connect.login.usuario_connect
+        self.senha = connect.login.usuario_connect
+
+
+        self.usr = self.user.get()
+        self.pw = self.passw.get()
+
+        self.sesion_usr = False
+        self.sesion_pw = False
+
+        try:
+            if self.usr == self.usuario:
+                self.sesion_usr = True
+            if self.pw == self.senha:
+                self.sesion_pw = True
+            else:
+                print('Usuário ou Senha errada')
+        except:
+            pass
+
+        if self.sesion_usr == True:
+            print('user ok')
+        if self.sesion_pw == True:
+            print('senha ok')
+            self.janela.destroy()
+
+
 
 app()
